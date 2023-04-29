d94bc38abc117e70b417303ad87d3cd921edefd3
hasan <hsnakbba@gmail.com>
2023-04-29 09:44:08 -0400

    credentials tested

---
+++ src/androidApp/PerformanceTesting.java
@@ -0,0 +1,9 @@
+package androidApp;
+
+public class PerformanceTesting {
+
+    // our test case is ready to send into testing stage
+    // below is our new changer on this project
+    // test login page function-
+    // test credentials
+}
