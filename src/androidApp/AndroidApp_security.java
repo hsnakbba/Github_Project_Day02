03abf0ea5c78c6ebcd9d2cb87fdf7c917014829c
hasan <hsnakbba@gmail.com>
2023-04-29 09:27:54 -0400

    created android home page

---
+++ src/androidApp/AndroidApp_security.java
@@ -0,0 +1,13 @@
+package androidApp;
+
+public class AndroidApp_security {
+
+    public static void main(String[] args) {
+
+       // lets create our neww app as below
+
+        // first thing first
+
+
+    }
+}
